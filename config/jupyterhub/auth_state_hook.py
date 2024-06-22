3648ed8dc344ae1545e9b81e55376cd82b9a2170
João Agostinho de Sousa <48960596+jpadesousa@users.noreply.github.com>
2024-06-22 20:58:50 +0200

    Linting the code

--- config/jupyterhub/auth_state_hook.py
+++ config/jupyterhub/auth_state_hook.py
@@ -9,14 +9,23 @@ def auth_state_hook(spawner, auth_state):
     # Get the uidNumber from the auth_state
     uid = os.getenv("DOCKER_NOTEBOOK_USERID")
     if not uid:
-        uid = auth_state.get('uidNumber', [1000])[0]
+        uid = auth_state.get("uidNumber", [1000])[0]
         # Default to 1000 if uidNumber is not in auth_state
 
     # Get the gidNumber from the auth_state
     gid = os.getenv("DOCKER_NOTEBOOK_GROUPID")
     if not gid:
-        gid = auth_state.get('gidNumber', [1000])[0]
+        gid = auth_state.get("gidNumber", [1000])[0]
         # Default to 1000 if gidNumber is not in auth_state
 
-    spawner.environment.update({'USERID': str(uid), 'GROUPID': str(gid), 'NB_UID': str(uid), 'NB_GID': str(gid)})
-    spawner.log.info(f"Setting USERID and NB_UID to {uid} and GROUPID and NB_GID to {gid} for user {spawner.user.name}")
+    spawner.environment.update(
+        {
+            "USERID": str(uid),
+            "GROUPID": str(gid),
+            "NB_UID": str(uid),
+            "NB_GID": str(gid),
+        }
+    )
+    spawner.log.info(
+        f"Setting USERID and NB_UID to {uid} and GROUPID and NB_GID to {gid} for user {spawner.user.name}"
+    )
